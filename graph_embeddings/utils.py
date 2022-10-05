a8bf75b457553d6f651c0d5033f0c8bf0674f700
sachinsharma9780 <sachinsharma9780@gmail.com>
2022-10-06 03:30:00 +0530

    moving downstream tasks from util to downstream_tasks folder

--- graph_embeddings/utils.py
+++ graph_embeddings/utils.py
@@ -1,17 +1,12 @@
 # utils file
 import torch.nn as nn
-import umap
-import matplotlib.pyplot as plt
-import seaborn as sns
 from arango import ArangoClient
 import torch
 from torch_geometric.data import Data
 from tqdm import tqdm
-import math
 import numpy as np
 import torch_geometric.transforms as T
 from adbpyg_adapter import ADBPyG_Adapter
-import faiss
 from rich.progress import track    
         
 
@@ -34,8 +29,14 @@ class GraphUtils(nn.Module):
             self.graph = self.pyg_preprocess(pyg_graph)
             print(self.graph_stats())
 
-    # Takes PyG data object and preprocess it
     def pyg_preprocess(self, pyg_data):
+        """Takes PyG data object and preprocess it. By default it performs following preporocessing:
+        1. AddMetaPaths
+        2. Perform Random Node Split i.e. splitting data into train, val and test set.
+        
+        :pyg_data (tpye: PyG data object): PyG data object
+        """
+
         if self.key_node!= None and self.metapaths!= None:     
             if hasattr(pyg_data[self.key_node], 'train_mask'):
                 pyg_data = pyg_data
@@ -51,12 +52,6 @@ class GraphUtils(nn.Module):
                 pyg_data = T.RandomNodeSplit(num_val=self.num_val, num_test=self.num_test)(pyg_data)
 
         else:
-            if pyg_data.has_isolated_nodes():
-                print('Removing isolated nodes.....')
-                pyg_data = T.RemoveIsolatedNodes()(pyg_data) 
-            else:
-                pyg_data = pyg_data
-
             if hasattr(pyg_data, 'train_mask'):
                 pyg_data = pyg_data
             else:
@@ -64,8 +59,18 @@ class GraphUtils(nn.Module):
        
         return pyg_data
 
-    # export ArangoDB graph to Pyg data object
+
     def arango_to_pyg(self, arango_graph, metagraph):
+        """Exports ArangoDB graph to PyG data object using ArangoDB PyG Adapter.
+        
+        :arango_graph (type: str): The name of ArangoDB graph which we want to export to PyG.
+        :metagraph (type: dict): It exports ArangoDB graphs to PyG data objects. We define metagraph as 
+            a dictionary defining vertex & edge collections to import to PyG, along 
+            with collection-level specifications to indicate which ArangoDB attributes will become PyG features/labels. 
+            It also supports different encoders such as identity and categorical encoder on database attributes. Detailed information regarding different
+            use cases and metagraph definitons can be found on adbpyg_adapter github page i.e <https://github.com/arangoml/pyg-adapter>.
+        """
+
         adbpyg = ADBPyG_Adapter(self.database)
         pyg_obj = adbpyg.arangodb_to_pyg(arango_graph, metagraph)
         pyg_obj = self.pyg_preprocess(pyg_obj)
@@ -137,98 +142,6 @@ class GraphUtils(nn.Module):
             g_info['node stats'] = node_data
             return g_info
             
-
-    def visualize_embeddings(self, graph_emb, class_mapping=None, node_type=None, emb_percent=0.1):
-        """Performs dimensionality reduction (2D) and visualization of graph embeddings using U-Map
-        
-        :graph_emb (type: 2D numpy array): Numpy array of size (n, embedding_size),
-        n: number of nodes in graph
-        embedding_size: Length of the node embeddings when they are mapped to d-dimensional euclidean space.
-        :class_mapping (type: dict): It is a dictionary where class names are mapped to integer labels.
-        If class_mappings are not provided the method uses integers labels as legend inside the figure. for e.g.
-        {0: 'Desktops',1: 'Data Storage',2: 'Laptops',3: 'Monitors',4: 'Computer Components',
-        5: 'Video Projectors',6: 'Routers',7: 'Tablets',8: 'Networking Products',9: 'Webcams'} # amazon computer dataset.
-        :node_type (type: str):  Node type for which we want to retreive embeddings to perform visualization. Used to store Hetero graph embeddings.
-        :emb_perc (type: float): Percentage of embeddings to visualize, 0.1 means 10% of data will be visualized.
-        """
-
-        if node_type!= None:
-            y_np = self.graph[node_type].y.cpu().numpy()
-        else:
-            y_np = self.graph.y.cpu().numpy()
-            
-        num_nodes = math.floor(graph_emb.shape[0] * emb_percent)
-        num_y = math.floor(y_np.shape[0] * emb_percent)
-        graph_emb = graph_emb[:num_nodes]
-        labels = y_np[:num_y]
-        umap_embd = umap.UMAP().fit_transform(graph_emb)
-        plt.figure(figsize=(8, 8))
-        if class_mapping!=None:
-            palette = {}
-            class_names = [class_mapping[l] for l in labels]
-            for n, y in enumerate(set(np.array(class_names))):
-                palette[y] = f'C{n}'
-            sns.scatterplot(x=umap_embd.T[0], y=umap_embd.T[1], hue=np.array(class_names), palette=palette)
-            plt.legend(bbox_to_anchor=(1,1), loc='upper left')
-            plt.savefig("./umap_embd_visualization.png", dpi=120)
-        else:
-            palette = {}
-            for n, y in enumerate(set(labels)):
-                palette[y] = f'C{n}'
-            sns.scatterplot(x=umap_embd.T[0], y=umap_embd.T[1], hue=labels, palette=palette)
-            plt.legend(bbox_to_anchor=(1,1), loc='upper left')
-            plt.savefig("./umap_embd_visualization.png", dpi=120)
-            
-
-    def similarity_search(self, graph_emb, top_k_nbors=10, nlist=10, search_type='exact'):
-        """Performs similarity search in sets of vectors of any size, up to ones that possibly do not fit in RAM using FAISS 
-        <https://engineering.fb.com/2017/03/29/data-infrastructure/faiss-a-library-for-efficient-similarity-search/>. 
-
-        returns dist, nbors
-            nbors: node ids with similar embeddings
-            dist: corresponding similarity scores
-        
-        :graph_emb (type: 2D numpy array): Numpy array of size (n, embedding_size),
-        n: number of nodes in graph
-        embedding_size: Length of the node embeddings when they are mapped to d-dimensional euclidean space.
-        :top_k_nbors (type: int): Returns top-k nearest neighbors of all embeddings.
-        :nlist (type: int): Number of clusters to be generated.
-        :search_type (type: str): We support two types of search for now:
-        1. exact search: For precise similarity search but at the cost of scalability.
-        2. approx search: For scalable similarity search but at the cost of some precision loss.
-        """
-
-        emb_dim = graph_emb.shape[1]
-        # exact search
-        if search_type == 'exact':
-            # inner product search index
-            search_index = faiss.IndexFlatIP(emb_dim)
-            # normalize the embeddings and add to search index
-            faiss.normalize_L2(graph_emb)
-            search_index.add(graph_emb)
-            # search top-k nearest neighbors of all embeddings
-            dist, nbors = search_index.search(graph_emb, k=top_k_nbors+1)
-
-        # approx search
-        elif search_type == 'approx':
-            # assign embeddings to specific cluster using Inner product
-            quantiser = faiss.IndexFlatIP(emb_dim)
-            # L2 normalization of embeddings
-            faiss.normalize_L2(graph_emb)
-            # creates partitioned search index
-            search_index = faiss.IndexIVFFlat(quantiser, emb_dim, nlist, faiss.METRIC_INNER_PRODUCT)
-            # train and add the embeddings to the index
-            search_index.train(graph_emb)
-            search_index.add(graph_emb)
-            # nbors: top k node ids with highest cosine sim
-            # dist: cosine distances of top k nodes
-            dist, nbors = search_index.search(graph_emb, k=top_k_nbors+1)
-
-        else:
-            assert search_type == 'exact' or search_type == 'approx', "pass search type either exact or approx"
-
-        return dist, nbors
-
     
     # store graph embeddings inside arangodb
     def store_embeddings(self, graph_emb, collection_name=None, batch_size=100, class_mapping=None, 

72de5dad2a83f00910181f6528d54805a42d353b
sachinsharma9780 <sachinsharma9780@gmail.com>
2022-10-21 18:54:30 +0530

    update

--- tests/test_ge_models.py
+++ tests/test_ge_models.py
@@ -26,6 +26,10 @@ def test_sage():
     # check vis
     class_names = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g'}
     downstream_tasks.visualize_embeddings(model.G, embeddings, class_mapping=class_names)
+    # check storing emb
+    if db.has_collection('cora_emb'):
+        db.delete_collection('cora_emb')
+    model.graph_util.store_embeddings(embeddings, collection_name='cora_emb', class_mapping=class_names, nearest_nbors_search=True)
 
 # test gat model
 def test_gat():

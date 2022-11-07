4e02aba7d57bc0e65cfc391fb206cbf9e9f0acfc
Chris Woodward <cw00dw0rd@gmail.com>
2022-11-07 12:58:11 -0500

    adds arango_datasets package

--- fastgraphml/__init__.py
+++ fastgraphml/__init__.py
@@ -1,6 +1,8 @@
+from arango_datasets.datasets import Datasets
+
 from fastgraphml.graph_embeddings.models.dmgi import DMGI
 from fastgraphml.graph_embeddings.models.gat import GAT
 from fastgraphml.graph_embeddings.models.graph_sage import SAGE
 from fastgraphml.graph_embeddings.models.metapath2vec import METAPATH2VEC
 
-__all__ = ["DMGI", "GAT", "SAGE", "METAPATH2VEC"]
+__all__ = ["DMGI", "GAT", "SAGE", "METAPATH2VEC", "Datasets"]
